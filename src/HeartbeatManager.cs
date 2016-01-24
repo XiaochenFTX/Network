045e5d1d879cecf779a3b872aa315edff306bf76
fingerpasswang <452934859@qq.com>
2016-01-24 14:37:00 +0800

    initial commit, add code

---
+++ src/HeartbeatManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Network
+{
+    class HeartbeatManager
+    {
+        private readonly int heartbeatInSeconds; //实际的心跳间隔，客户端发起间隔
+
+        private const int HeartbeatInterval = 5; //心跳间隔
+
+        private readonly byte[] clientHandshakeBuffer = new byte[4 + 4]; // 握手时候的客户端id buffer cache，[sign*5,clientId*4,heartbeat*4]
+        private int heartbeatInternalTimeout = 600; //内部网络心跳超时,服务器校验
+        private int heartbeatTimeout = 30; //网络心跳超时,服务器校验
+
+        public void SetHeartbeatTimeout(int timeout, int internalTimeout)
+        {
+            heartbeatTimeout = timeout <= HeartbeatInterval ? HeartbeatInterval * 3 : timeout;
+            heartbeatInternalTimeout = internalTimeout <= HeartbeatInterval ? HeartbeatInterval * 3 : internalTimeout;
+        }
+
+        //private DateTime lastCheckTime = UXTime.GetNowDateTime();
+
+        private void CheckHeartbeat()
+        {
+            //if (UXTime.GetNowDateTime().Subtract(lastCheckTime).TotalSeconds < 1) //心跳1秒算一次
+            //{
+            //    return;
+            //}
+
+            //lastCheckTime = UXTime.GetNowDateTime();
+
+            //// 如果有心跳超时的客户端，在这里判断处理
+            //foreach (var client in clientConnectorsDict)
+            //{
+            //    float timeout = client.Value.UserId > 0 ? heartbeatTimeout : heartbeatInternalTimeout;//内部socket超时时间和对外不同
+            //    if (timeout > 0 && nowTime > client.Value.LastHeartbeatTime.AddSeconds(timeout))
+            //    {
+            //        Close(client.Value, NetworkCloseMode.HeartbeatTimeout);//心跳超时，断开
+            //    }
+            //}
+        }
+    }
+}
---
+++ src/Log.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Network
+{
+    // internal log module 
+    // log handlers can be injected externally
+    // todo not ensure thread-safe yet
+    public static class Log
+    {
+        public delegate void LogHandler(string msg);
+
+        public static LogHandler InfoHandler;
+        public static LogHandler DebugHandler;
+        public static LogHandler WarnHandler;
+        public static LogHandler ErrorHandler;
+
+        enum LogLevel
+        {
+            Info,
+            Debug,
+            Warn,
+            Error,
+        }
+
+        internal static void Info(string formater, params object[] args)
+        {
+            LogInternal(LogLevel.Info, string.Format(formater, args));
+        }
+        internal static void Info(object msg)
+        {
+            LogInternal(LogLevel.Info, msg.ToString());
+        }
+        internal static void Debug(string formater, params object[] args)
+        {
+            LogInternal(LogLevel.Debug, string.Format(formater, args));
+        }
+        internal static void Debug(object msg)
+        {
+            LogInternal(LogLevel.Debug, msg.ToString());
+        }
+        internal static void Warn(string formater, params object[] args)
+        {
+            LogInternal(LogLevel.Warn, string.Format(formater, args));
+        }
+        internal static void Warn(object msg)
+        {
+            LogInternal(LogLevel.Warn, msg.ToString());
+        }
+        internal static void Error(string formater, params object[] args)
+        {
+            LogInternal(LogLevel.Error, string.Format(formater, args));
+        }
+        internal static void Error(object msg)
+        {
+            LogInternal(LogLevel.Error, msg.ToString());
+        }
+
+        static void LogInternal(LogLevel level, string msg)
+        {
+            LogHandler handler = null;
+
+            switch (level)
+            {
+                case LogLevel.Info:
+                    handler = InfoHandler;
+                    break;
+                case LogLevel.Debug:
+                    handler = DebugHandler;
+                    break;
+                case LogLevel.Warn:
+                    handler = WarnHandler;
+                    break;
+                case LogLevel.Error:
+                    handler = ErrorHandler;
+                    break;
+            }
+
+            if (handler == null)
+            {
+                return;
+            }
+
+            handler(msg);
+        }
+    }
+}
